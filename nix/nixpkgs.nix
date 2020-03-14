3e5802a512c75019c83c5971110455535185aa8f
Laurens Duijvesteijn <git@duijf.io>
2020-03-14 16:00:10 +0100

    nix: Update to latest nixpkgs-unstable

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,40 +1,9 @@
-# SYNOPSIS
-#
-#   Pinned nixpkgs version.
-#
-#   Enabled reproducibility, so users don't rely on the NIX_PATH and global
-#   state. This also allows this repo to be used when only Nix has been
-#   installed through the bin/bootstrap-nix.py installer and nixpkgs isn't
-#   available.
-#
-# USAGE
-#
-#   let
-#     # You can pass in arbtrary arguments to nixpkgs if you want.
-#     pkgs = import ./path/to/here/nixpkgs.nix {};
-#   in
-#     pkgs.stdenv.mkDerviation { .. }
-#
-# UPDATING
-#
-#   Replace the value of `nixpkgsRev` with the latest commit on GitHub and
-#   build something depending on it. Then read the error message and update the
-#   sha256 value.
 let
-  nixpkgsRev = "7a53b1cbe79d91167874eb4f0d4ccc69c2e45007";
-  nixpkgsTar = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgsRev}.tar.gz";
-    sha256 = "sha256:1mwxipvyrwfbl0z6mghv0gr6z61awidnpld1p8h4fwjfifv0dn2j";
-  };
-
-  mozOverlayRev = "d46240e8755d91bc36c0c38621af72bf5c489e13";
-  mozOverlayTar = fetchTarball {
-    url = "https://github.com/mozilla/nixpkgs-mozilla/archive/${mozOverlayRev}.tar.gz";
-    sha256 = "sha256:0icws1cbdscic8s8lx292chvh3fkkbjp571j89lmmha7vl2n71jg";
+  # Nixpkgs unstable on 2020-03-14
+  rev = "991bbef68351b4aa228f5f763c8d2ded2aeeb84e";
+  tarball = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    sha256 = "sha256:1d6byw99i4wjwdz501r6b12i8nifwl86gjd43cjvg8f2d78fazpg";
   };
 in
-  import "${nixpkgsTar}/default.nix" {
-    overlays = [
-      (import "${mozOverlayTar}/default.nix")
-    ];
-  }
+  import tarball
