e992fd2b08b879c7176635268c3fd4dc4caa59f4
Laurens Duijvesteijn <git@duijf.io>
2020-04-06 18:47:57 +0200

    Fix nixpkgs sha

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -3,7 +3,7 @@ let
   rev = "991bbef68351b4aa228f5f763c8d2ded2aeeb84e";
   tarball = fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    sha256 = "sha256:1d6byw99i4wjwdz501r6b12i8nifwl86gjd43cjvg8f2d78fazpg";
+    sha256 = "sha256:0wq4xqm0qliphc11sj08mp8586r039l6v1x1a2cpvb66sh36a54w";
   };
 in
   import tarball
